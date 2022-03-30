70ac06fb98960ea5a0e8be442682e7bd5184cec0
Olwanishola <lukusope@yahoo.com>
2022-03-30 06:48:01 -0700

    0x08-recursion/5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,40 @@
+#include "main.h"
+
 /**
- * recurse - find square root
- * @n: number to find square root of
- * @i: number of iterations
- * Return: square root or -1 if not found
+ * check_int - checks integer as candidate for square root
+ * @square: square integer
+ * @x: integer
+ * Return: 1 if
  */
-int recurse(int n, int i)
+
+int check_int(int square, int x)
 {
-	if (n == 0)
-		return (0);
-	else if (n < 0)
+	if (x * x == square)
+		return (x);
+	else if (x > square / x)
+		return (check_int(square, x - 1));
+	else if (x < square / x)
+		return (check_int(square, x + 1));
+	else
 		return (-1);
-	else if (n / i == i && (n % i == 0))
-		return (i);
-	else if (i < n)
-		return (recurse(n, i + 1));
-	return (-1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer
+ * Return: integer - square root, if none returns -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	int start;
+
+	start = 1;
+
+	if (n < 0)
+		return (-1);
+	else if (n == 1)
+		return (1);
+	else
+		return (check_int(n, start));
 }
