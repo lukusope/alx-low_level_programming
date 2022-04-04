8f3a52da138c5f454fd2a1efabb16afaf70eda62
Olwanishola <lukusope@yahoo.com>
2022-04-04 07:16:02 -0700

    0x0B-malloc_free/100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,46 +2,59 @@
 #include <stdlib.h>
 
 /**
-* *argstostr - concatenates all arguements to the program
-* @ac: arguement count
-* @av: pointer to arguements
-* Return: pointer to new space in memory or null
-**/
+ * _strlen - gets string length
+ * @string: string
+ * Return: length (int)
+ */
+
+int _strlen(char *string)
+{
+	int length;
+
+	length = 0;
+	while (string[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * argstostr - concatenates all the arguments to a program
+ * @ac: argc - number of arguments
+ * @av: argv - array of arguments
+ * Return: pointe to new string
+ */
+
 char *argstostr(int ac, char **av)
 {
-	char *strDup;
-	int i, j, k, size;
+	int length, i, j, k;
+	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	size = 0;
-	/* count the number of chars in each string */
+	length = 0;
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			size++;
-		size++;
+		length += _strlen(av[i]) + 1;
 	}
-	size++;
 
-	 /* allocate memory for total number of chars and
-	 * new line for each word
-	 */
-	strDup = malloc(sizeof(char) * size);
-
-	if (strDup == NULL)
+	str = malloc(sizeof(char) * length + 1);
+	if (str == NULL)
 		return (NULL);
 
 	k = 0;
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; j < _strlen(av[i]); j++)
 		{
-			strDup[k++] = av[i][j];
+			str[k] = av[i][j];
+			k++;
 		}
-		strDup[k++] = '\n';
+		str[k] = '\n';
+		k++;
 	}
-	strDup[k] = '\0';
-	return (strDup);
+	str[k] = '\0';
+	return (str);
 }
